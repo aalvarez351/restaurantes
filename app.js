13b1ceed83fc3b41d84e45e7173ad9b0f759467c
aalvarez351 <aalvarez351@gmail.com>
2026-03-04 23:46:16 -0500

    first commidt

---
+++ app.js
@@ -0,0 +1,33 @@
+
+async function loadMenu(){
+
+const res = await fetch("menu.json")
+const data = await res.json()
+
+const panel = document.getElementById("menuPanel")
+
+data.items.forEach(item=>{
+
+let div = document.createElement("div")
+div.className="item"
+
+div.innerHTML=`
+<b>${item.name}</b> - ${item.price}<br>
+${item.description}<br>
+<button onclick="order('${item.name}','${data.phone}')">Ordenar</button>
+`
+
+panel.appendChild(div)
+
+})
+
+}
+
+function order(item,phone){
+
+const url=`https://wa.me/${phone}?text=Quiero%20ordenar%20${encodeURIComponent(item)}`
+window.open(url)
+
+}
+
+loadMenu()
